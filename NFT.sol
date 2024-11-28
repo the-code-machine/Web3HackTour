b977cba29916c8a641c0189ce8bd88609c42aaf3
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-11-28 14:54:47 +0530

    Update

--- NFT.sol
+++ NFT.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NameNFT is ERC721 {
+    uint256 private tokenIdCounter = 1;
+    mapping(uint256 => string) private tokenIdToName;
+    address private ownerAddress;
+
+    constructor() ERC721("NameNFT", "NNFT") {
+        ownerAddress = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == ownerAddress, "Not the contract owner");
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        ownerAddress = _newOwner;
+    }
+
+    function mintWithName(string memory _name) external onlyOwner {
+        uint256 tokenId = tokenIdCounter++;
+        _safeMint(ownerAddress, tokenId);
+        tokenIdToName[tokenId] = _name;
+    }
+
+    function getName(uint256 tokenId) external view returns (string memory) {
+        return tokenIdToName[tokenId];
+    }
+}
 
